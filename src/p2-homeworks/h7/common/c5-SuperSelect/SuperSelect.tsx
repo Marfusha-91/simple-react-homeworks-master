caf5a2136f64fbbb850bedaf378e605f99c000cd
Marfusha-91 <rovena91@mail.ru>
2022-04-29 21:26:52 +0300

    done homework7-8

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,4 +1,5 @@
 import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+import s from './SuperSelect.module.css'
 
 type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
 
@@ -9,14 +10,19 @@ type SuperSelectPropsType = DefaultSelectPropsType & {
 
 const SuperSelect: React.FC<SuperSelectPropsType> = (
     {
-        options,
+        options,className,
         onChange, onChangeOption,
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+    const mappedOptions: any[] = options ? options.map((o, i) => (
+     <option key={i} value={o}>{o}</option>
+    )) : []; // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
         // onChange, onChangeOption
     }
 
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,31 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType = {type:'sort', payload: 'up' | 'down'} | {type:'check'}
+
+
+
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
-        }
+         let newSort = [...state].sort( function(a, b)  {
+                /* if(a.name > b.name)
+                  return 1
+
+                else if(a.name < b.name)
+                     return -1
+                 else return 0*/
+
+             return a.name > b.name ? 1 : a.name < b.name ? -1 : 0
+
+        })
+
+         return action.payload === 'up' ? newSort : newSort.reverse()
+        };
         case 'check': {
             // need to fix
-            return state
+            return [...state].filter(el => el.age >= 18)
         }
         default: return state
     }
